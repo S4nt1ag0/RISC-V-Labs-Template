1be7f233d0e0da425f481b5fec0b9ee8566eb03d
S4nt1ag0 <gustavosantiago1018@gmail.com>
2025-04-11 10:10:56 -0300

    lab8 recursive

--- labs/lab08/lab08.s
+++ labs/lab08/lab08.s
@@ -1,44 +1,34 @@
-.section .rodata
-fmt:    .string "%d\n"
-
 .section .text
 
 .global func
 
 func:
-    li t0, 0          # Fibonacci[0]
-    li t1, 1          # Fibonacci[1]
-    mv t2, a0         # Número de elementos a imprimir
-    li t3, 0          # Índice
+    li t0, 2
+    bge a0, t0, RECURSIVE
+    ret
 
-    # Imprimir o primeiro número
-    la a0, fmt
-    mv a1, t0
-    call printf
+RECURSIVE:
+    addi sp, sp, -24 #aumenta o tamanho da pilha
+    sd ra, 16(sp)    #salva o endereço de retorno da função
+    sd s1, 8(sp)     #salva o valor do registrador s1 Fibonacci[n-1]
+    sd s0, 0(sp)     #salva o valor do registrador s0 Fibonacci[n-2]
 
-    addi t3, t3, 1
-    beq t3, t2, end
+    mv s0, a0        #extrai o valor do argumento passado como parametro
 
-    # Imprimir o segundo número
-    la a0, fmt
-    mv a1, t1
-    call printf
+    #s1 = f(n-1)
+    addi a0, s0, -1
+    call func
+    mv s1, a0       
+    
+    #s2 = f(n-2)
+    addi a0, s0, -2
+    call func
+    add a0, s1, a0
 
-    addi t3, t3, 1
-    beq t3, t2, end
 
-
-loop:
-    add t4, t0, t1    # t4 = t0 + t1 (próximo Fibonacci)
-    mv t0, t1         # t0 = t1
-    mv t1, t4         # t1 = próximo Fibonacci
-
-    la a0, fmt
-    mv a1, t4
-    call printf
-
-    addi t3, t3, 1
-    blt t3, t2, loop
-
-end:
+    ld ra, 16(sp)   #restaura o endereço de retorno para o registrador
+    ld s1, 8(sp)    #restaura o valor de Fibonacci[n-1] salvo em memoria para o registrador
+    ld s0, 0(sp)    #restaura o valor de Fibonacci[n-2] salvo em memoria para o registrador
+    addi sp, sp, 24 #restaura a pilha
+    
     ret
--- labs/lab08/main.c
+++ labs/lab08/main.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 
-extern void func(int fibonacci_number);
+extern int func(int fibonacci_number);
 
-int main(){
-    int fibonacci_number = 6;
-    func(fibonacci_number);
+int main() {
+    int fibonacci_number = 9;
+
+    printf("Sequência Fibonacci com %d termos:\n", fibonacci_number);
+    for (int i = 0; i < fibonacci_number; i++) {
+        int result = func(i);
+        printf("%d ", result);
+    }
+
+    printf("\n");
     return 0;
 }
